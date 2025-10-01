bde4aaea09e0b9534e3e1e8721fbf171005590b2
CooLyan12 <doctorusieng@gmail.com>
2025-10-01 09:21:57 +0800

    init commit у меня мама серьорит

--- js.js
+++ js.js
@@ -1,11 +1,32 @@
 window.onload = function () {
     let inputField = document.getElementById('inputField')
-    let butDigits= document.querySelectorAll('.digits-wrap button')
-
+    let butDigits = document.querySelectorAll('.digits-wrap button')
+    let num1 = 0;
+    let clean = 0;
+    let butMathOperation = document.querySelectorAll('.math-wrap button')
 
     butDigits.forEach(function (el) {
-        el.addEventListener('click', function (e){
-            console.log(e.target.innerText)
+        el.addEventListener('click', function (e) {
+            console.log(e.target.innerText);
+            inputField.value = inputField.value + e.target.innerText;
+            if (clean === 1) {
+                inputField.value = '';
+                clean = 0;
+
+            }
+        })
     })
-    })
+
+
+    butMathOperation.forEach(
+        (el) => {
+            el.addEventListener('click', (e) => {
+                num1 = inputField.value;
+                clean = 1
+                console.log(e.target.innerText)
+
+            })
+        }
+    )
+
 }
